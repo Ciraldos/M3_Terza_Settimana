53607224e9754d94e9798d0b36d40342a2d9da5a
Ciraldos <andreaciraldo2@gmail.com>
2024-06-03 17:05:59 +0200

    .

--- Giorno_1/Esercizio_S11_L1/src/app/post-detail/post-detail-routing.module.ts
+++ Giorno_1/Esercizio_S11_L1/src/app/post-detail/post-detail-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PostDetailComponent } from './post-detail.component';
+import { RouterLink, RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: PostDetailComponent
+  }
+]
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class PostDetailRoutingModule { }
--- Giorno_1/Esercizio_S11_L1/src/app/post-detail/post-detail.module.ts
+++ Giorno_1/Esercizio_S11_L1/src/app/post-detail/post-detail.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PostDetailComponent } from './post-detail.component';
+import { SharedModule } from '../shared/shared/shared.module';
+import { PostDetailRoutingModule } from './post-detail-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PostDetailComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    PostDetailRoutingModule
   ]
 })
 export class PostDetailModule { }
